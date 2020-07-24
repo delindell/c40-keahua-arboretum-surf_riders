c3fc3991e0c1d650887df27d3c4b169a956ec380
felipetempus <felipemouradev@gmail.com>
2020-07-24 15:39:38 -0400

    biomes corrected

--- environments/forest.py
+++ environments/forest.py
@@ -2,6 +2,6 @@ from . import Biome
 
 class Forest(Biome):
     def __init__(self, name,):
-        super().__init__(name, 3, 3)
+        super().__init__(name, 20, 32)
         self.rainy = True
         self.shady = True
