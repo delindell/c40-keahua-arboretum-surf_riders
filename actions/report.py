042069774e3892a2a9f81b9aa05dfb71e739420c
zanebliss <zanebliss@icloud.com>
2020-07-24 09:56:12 -0500

    modified report.py

--- actions/report.py
+++ actions/report.py
@@ -2,12 +2,13 @@ import os
 
 def build_facility_report(arboretum):
     os.system('cls' if os.name == 'nt' else 'clear')
-    for key, value in arboretum.habitats.items():
-        print(key, value)
-        for item in value:
+    for key, habitats in arboretum.habitats.items():
+        # print(key, habitats)
+        for item in habitats:
+            print(f'\n{item.name} [{item.id}]')
             for index, animal in enumerate(item.animal_population):
-                print(f'  {index + 1}. {animal.species}')
+                print(f'  {index + 1}. {animal.species} ({animal.id})')
             for index, plant in enumerate(item.plant_population):
-                print(f'  {index + 1}. {plant.species}')
+                print(f'  {index + 1}. {plant.species} ({plant.id})')
 
     input("\n\nPress any key to continue...")
